b8e0197c25926f158c5721d66c0c67bce65fbada
Daniel Nyvoll <daniel@escali.no>
2024-02-02 16:18:03 +0100

    Make build work

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import { RigidBody, vec3 } from "@react-three/rapier";
+import { RigidBody } from "@react-three/rapier";
 import { Box } from "@react-three/drei";
 import { calculateMovementImpulse } from '../utils/physics';
 import { useEffect, useRef } from 'react';
@@ -7,48 +7,41 @@ import { useEffect, useRef } from 'react';
 const Player = ({ onCollisionEnter, onCollisionExit, command }) => {
     const playerRef = useRef();
     console.log(command); 
-    let max_speed = 7.5;
-   // let acceleration = 0.5;
 
-    let impulse = vec3({ x: 0, y: 0, z: 0 });
         
-    const executeCommand = (command) => {
-        
-        let directions = { forward: false, backward: false, left: false, right: false };
-
-        switch (command.toLowerCase()) {
-            case "up":
-                directions.forward = true;
-                break;
-            case "down":
-                directions.backward = true;
-                break;
-            case "left":
-                directions.left = true;
-                break;
-            case "right":
-                directions.right = true;
-                break;
-        }
-       
-        console.log(directions);
-        impulse = calculateMovementImpulse(directions, 0.5, 7.5, playerRef);
-        
-        let current_velocity = vec3(playerRef.current.linvel());
-        let current_speed = current_velocity.length();
-        let max_speed_factor = max_speed / Math.max(current_speed, 0.5);
-
-        if (current_speed > max_speed) {
-            playerRef.current.setLinvel(current_velocity.multiplyScalar(max_speed_factor), true);
-        }
-        playerRef.current.applyImpulse(impulse, true);
-    };
-
     useEffect(() => {
+        const executeCommand = (cmd) => {
+            let directions = { forward: false, backward: false, left: false, right: false };
+    
+            switch (cmd.toLowerCase()) {
+                case "up":
+                    directions.forward = true;
+                    break;
+                case "down":
+                    directions.backward = true;
+                    break;
+                case "left":
+                    directions.left = true;
+                    break;
+                case "right":
+                    directions.right = true;
+                    break;
+                default:
+                    // Handle unexpected commands (e.g., log an error message)
+                    console.error(`Unexpected command: ${cmd}`);
+                    break;
+                // Add more cases if needed
+            }
+    
+            const impulse = calculateMovementImpulse(directions, 0.5, 7.5, playerRef);
+            playerRef.current.applyImpulse(impulse, true);
+        };
+    
         if (command) {
             executeCommand(command);
         }
     }, [command]);
+    
 
     return (
         <RigidBody
