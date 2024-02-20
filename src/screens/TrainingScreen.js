561fed5b869639de0078ccee82b9de79b3de5784
Thumsificial <92872386+Thumsificial@users.noreply.github.com>
2024-02-20 14:14:14 +0100

    Added full screen for soccer

--- src/screens/TrainingScreen.js
+++ src/screens/TrainingScreen.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Soccer from "../Soccer";
 import "./TrainingScreen.css";
 
 function TrainingScreen() {
+
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  const toggleFullScreen = () => {
+    setIsFullScreen(!isFullScreen);
+  }
+
   return (
     <div className="training-screen">
       <div className="left-container">
@@ -12,7 +19,10 @@ function TrainingScreen() {
       </div>
       <div className="right-container">
         <div className="component-container" style={{ width: "95%", height: "45%" }}>
-          <div className="game-container">
+          <div className={isFullScreen ? "full-screen" : "game-container"}>
+            <button onClick={toggleFullScreen}>
+              {isFullScreen ? "Exit Full Screen" : "Full Screen"}
+            </button>
             <Soccer />
           </div>
         </div>
