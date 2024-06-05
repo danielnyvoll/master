ee7cfdb821567be806f1c3ce4913f64c8cc1af7e
Daniel Nyvoll <daniel@escali.no>
2024-06-05 16:58:54 +0200

    Color on goals

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -11,11 +11,13 @@ import { MeshPhysicalMaterial } from "three";
 import { useDispatch, useSelector } from 'react-redux';
 import { setGoal } from '../store';
 
-const material = new MeshPhysicalMaterial();
-
 const Goal = ({ position, rotation, isBlue }) => {
   const dispatch = useDispatch();
   const intersecting = useSelector(state => state.goal.intersecting);
+
+  const materialColor = isBlue ? 'blue' : 'red';
+  const material = new MeshPhysicalMaterial({ color: materialColor });
+
   return (
     <RigidBody position={position} rotation={rotation} type='fixed'>
       <Box
@@ -37,9 +39,6 @@ const Goal = ({ position, rotation, isBlue }) => {
         castShadow
       />
 
-      
-      
-
       <Suspense fallback={null}>
         {intersecting && isBlue && (
           <Text color="red" position={[0, 5, 0]} fontSize={2}>
@@ -53,11 +52,6 @@ const Goal = ({ position, rotation, isBlue }) => {
         )}
       </Suspense>
 
-      {/**
-       * We create a collider and set it to be a 'sensor'
-       * This enables intersection events and enables
-       * colliders to pass through it.
-       */}
       <CuboidCollider
         position={[0, 0, -1]}
         args={[5, 3, 1]}
@@ -69,9 +63,7 @@ const Goal = ({ position, rotation, isBlue }) => {
             }
             else{
               dispatch(setGoal({intersecting: true, scoringSide: 1}));
-
             }
-            
           }  
         }}
         onIntersectionExit={() => {
